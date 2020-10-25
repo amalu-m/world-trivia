86a36f44943fae1f25bcdad5fc59055adf66e9a3
Amalu Mathew <amalulmathew@gmail.com>
2020-10-25 00:48:44 -0700

    added allowed host

--- quiz/settings/prod.py
+++ quiz/settings/prod.py
@@ -25,7 +25,7 @@ SECRET_KEY = base.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.pythonanywhere.com']
 
 
 # Application definition
