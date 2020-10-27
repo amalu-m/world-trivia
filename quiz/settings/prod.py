f95571d36f54c9b1669369bad99e4552076849d3
Amalu Mathew <amalulmathew@gmail.com>
2020-10-27 09:54:48 -0700

    allowed host is added and america.csv file added to database

--- quiz/settings/prod.py
+++ quiz/settings/prod.py
@@ -25,7 +25,7 @@ SECRET_KEY = base.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.pythonanywhere.com']
+ALLOWED_HOSTS = ['amalulinto.pythonanywhere.com']
 
 
 # Application definition
