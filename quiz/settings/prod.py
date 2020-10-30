11d49099c3a984f86ac8cc0d6604cc94259cdd51
Amalu Mathew <amalulmathew@gmail.com>
2020-10-30 12:01:04 -0700

    allowed host is changed to tripbuddy.info

--- quiz/settings/prod.py
+++ quiz/settings/prod.py
@@ -25,7 +25,7 @@ SECRET_KEY = base.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['amalulinto.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.tripbuddy.info']
 
 
 # Application definition
